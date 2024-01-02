c8c7401466e6587015a7603b49031c24e9c79764
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-01-02 09:35:54 +0100

    Job1 et Job2 terminé

---
+++ jour01/job01.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Cyril Génisson
+
+@file: job01.py
+@created: 02/01/2024
+
+@project: 
+@licence: GPLv3
+"""
+
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+if __name__ == "__main__":
+    op = Operation(12, 3)
+    print(op)
---
+++ jour01/job02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Cyril Génisson
+
+@file: job01.py
+@created: 02/01/2024
+
+@project: 
+@licence: GPLv3
+"""
+
+class Operation:
+    def __init__(self, nombre1, nombre2):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+if __name__ == "__main__":
+    op = Operation(12, 3)
+    print(f"Le nombre1 est {op.nombre1}")
+    print(f"Le nombre2 est {op.nombre2}")
+
