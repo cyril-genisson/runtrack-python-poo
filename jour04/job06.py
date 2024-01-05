82aa3357ad59493255b8fce66ff26454bdb82409
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-01-05 05:38:40 +0100

    Job 6 terminé

--- jour04/job06.py
+++ jour04/job06.py
@@ -42,6 +42,9 @@ class Vehicule:
     def set_price(self, price):
         self.__price = price
 
+    def demarrer(self):
+        print("Attention, je roule")
+
 
 class Voiture(Vehicule):
     def __init__(self, brand, model, year, price, doors=4):
@@ -61,7 +64,37 @@ class Voiture(Vehicule):
         print(f"Prix = {self.get_price()}")
         print(f"Nombre de portes = {self.get_doors()}")
 
+    def demarrer(self):
+        print(f"Je roule avec {self.get_doors()} portes")
+
+
+class Moto(Vehicule):
+    def __init__(self, brand, model, year, price, wheels=2):
+        super().__init__(brand = brand, model=model, year=year, price=price)
+        self.__wheels = wheels
+
+    def get_wheels(self):
+        return self.__wheels
+
+    def set_wheels(self, wheels):
+        self.__wheels = wheels
+
+    def informationsVehicule(self):
+        print(f"Marque = {self.get_brand()}")
+        print(f"Modèle = {self.get_model()}")
+        print(f"Année = {self.get_year()}")
+        print(f"Prix = {self.get_price()}")
+        print(f"Nombre de roues = {self.get_wheels()}")
+
+    def demarrer(self):
+        print(f"Je roule avec {self.get_wheels()} roues")
+
 
 if __name__ == "__main__":
     voiture1 = Voiture("Mercedes", "Classe A", 2020, 18500)
+    moto1 = Moto("Yamaha", "1200 Vmax", 1987, 4500)
     voiture1.informationsVehicule()
+    voiture1.demarrer()
+    print()
+    moto1.informationsVehicule()
+    moto1.demarrer()
