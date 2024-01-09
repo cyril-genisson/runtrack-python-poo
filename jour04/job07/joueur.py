9b5fc3796e4655eb9f401801fdf4bb64a292e8e8
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-01-09 11:35:02 +0100

    Retrait PDF

---
+++ jour04/job07/joueur.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: Cyril Génisson
+
+@file: joueur.py
+@created: 07/01/2024
+
+@project: 
+@licence: GPLv3
+"""
+from constantes import *
+from carte import Carte
+
+
+class Joueur:
+    self __init__(self, name):
+        self.__name = name
+        self.__monnaie = DEM_MONNAIE
+        self.__cartes  = []
+
+
